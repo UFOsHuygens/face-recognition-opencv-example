2fc5e2428af11b32d6fd7337a0520f02ac0de75f
Felipe Vieira <87919419+UFOsHuygens@users.noreply.github.com>
2021-08-12 18:01:44 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-cap.set(10, 100)
 
 while True:
-    faceCascade = cv2.CascadeClassifier("face.xml")
+    cascade = cv2.CascadeClassifier("xml/palm.xml")
     success, img = cap.read()
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
-    for (x, y, w, h) in faces:
+    objects = cascade.detectMultiScale(imgGray, 1.1, 4)
+    for (x, y, w, h) in objects:
         cv2.rectangle(img,(x, y), (x+w, y+h), (255, 0, 0), 2)
     #    print('detected')
     
